32d911cfcb644de8757983b472087af40fdae667
Francisco Ramírez <frankjose2004@gmail.com>
2025-08-03 16:48:34 -0500

    REFACTOR: Pagination logic extracted to custom hook;

--- frontend/src/components/organisms/PatientsTable/PatientsTable.tsx
+++ frontend/src/components/organisms/PatientsTable/PatientsTable.tsx
@@ -1,107 +1,31 @@
-import { Dispatch, SetStateAction } from 'react'
-
-import { IPatient } from '@interfaces'
+import { usePagination, usePatients } from '@hooks'
 
 import { IcChevron, IcSpinner } from '@components/atoms'
 
-const handlePageChange = ({
-	page,
-	totalPages,
-	setCurrentPage,
-}: {
-	page: number
-	totalPages: number
-	setCurrentPage: Dispatch<SetStateAction<number>>
-}) => {
-	if (page > 0 && page <= totalPages) {
-		setCurrentPage(page)
-	}
-}
+export const PatientsTable = () => {
+	const {
+		patients,
+		patientsCount,
+		patientsPerPage,
+		currentPage,
+		setCurrentPage,
+		isLoading,
+	} = usePatients()
 
-const dynamicPagination = ({
-	currentPage,
-	totalPages,
-	setCurrentPage,
-}: {
-	currentPage: number
-	totalPages: number
-	setCurrentPage: Dispatch<SetStateAction<number>>
-}) => {
-	const maxPagesToShow = 5
-	const startPage = Math.max(
-		Math.min(
-			currentPage - Math.floor(maxPagesToShow / 2),
-			totalPages - maxPagesToShow + 1
-		),
-		1
-	)
-	const endPage = Math.min(startPage + maxPagesToShow - 1, totalPages)
-
-	const pageSetToDisplay = Array.from(
-		{ length: endPage - startPage + 1 },
-		(_, idx) => startPage + idx
-	)
-
-	const paginationItems = pageSetToDisplay.map(pageNumber => (
-		<li
-			key={pageNumber}
-			className={`${
-				currentPage === pageNumber
-					? 'bg-primary text-white'
-					: 'hover:bg-gray-100'
-			}`}
-			onClick={() =>
-				handlePageChange({ page: pageNumber, totalPages, setCurrentPage })
-			}
-		>
-			{pageNumber}
-		</li>
-	))
-
-	if (endPage < totalPages) {
-		paginationItems.push(
-			<li key='dots' className='!cursor-default'>
-				...
-			</li>
-		)
-		paginationItems.push(
-			<li
-				key={totalPages}
-				className={`hover:text-primary hover:bg-primary/5 ${
-					currentPage === totalPages ? 'text-tertiary bg-primary' : ''
-				}`}
-				onClick={() =>
-					handlePageChange({ page: totalPages, totalPages, setCurrentPage })
-				}
-			>
-				{totalPages}
-			</li>
-		)
-	}
-
-	return paginationItems
-}
-
-interface IPatientsTableProps {
-	patients: IPatient[]
-	patientsCount: number
-	patientsPerPage: number
-	currentPage: number
-	setCurrentPage: Dispatch<SetStateAction<number>>
-	isLoading: boolean
-}
-
-export const PatientsTable = ({
-	patients,
-	patientsCount,
-	patientsPerPage,
-	currentPage,
-	setCurrentPage,
-	isLoading,
-}: IPatientsTableProps) => {
 	const totalPages = patientsCount
 		? Math.ceil(patientsCount / patientsPerPage)
 		: 1
+
+	const {
+		handlePageChange,
+		renderPaginationItems,
+		canGoPreviousPage,
+		canGoNextPage,
+	} = usePagination({
+		currentPage,
+		setCurrentPage,
+		totalPages,
+	})
 
 	return (
 		<section className='flex size-full flex-col items-center'>
@@ -162,30 +86,18 @@ export const PatientsTable = ({
 				<ul className='mt-5 flex min-h-12 max-w-[80%] items-center justify-between gap-x-3.5 overflow-x-auto overflow-y-hidden rounded-md bg-white px-4 py-2.5 text-black shadow-lg *:flex *:size-6 *:shrink-0 *:items-center *:justify-center *:rounded *:hover:cursor-pointer'>
 					<li
 						className={`hover:bg-gray-100 ${
-							currentPage === 1 && '!cursor-not-allowed opacity-50'
+							!canGoPreviousPage && '!cursor-not-allowed opacity-50'
 						}`}
-						onClick={() =>
-							handlePageChange({
-								page: currentPage - 1,
-								totalPages,
-								setCurrentPage,
-							})
-						}
+						onClick={() => handlePageChange(currentPage - 1)}
 					>
 						<IcChevron className='size-3.5 -rotate-90 fill-black' />
 					</li>
-					{dynamicPagination({ currentPage, totalPages, setCurrentPage })}
+					{renderPaginationItems()}
 					<li
 						className={`hover:bg-gray-100 ${
-							currentPage === totalPages && '!cursor-not-allowed opacity-50'
+							!canGoNextPage && '!cursor-not-allowed opacity-50'
 						}`}
-						onClick={() =>
-							handlePageChange({
-								page: currentPage + 1,
-								totalPages,
-								setCurrentPage,
-							})
-						}
+						onClick={() => handlePageChange(currentPage + 1)}
 					>
 						<IcChevron className='size-3.5 rotate-90 fill-black' />
 					</li>
