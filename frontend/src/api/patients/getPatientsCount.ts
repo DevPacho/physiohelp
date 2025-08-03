61f8b120023319f78cafc7798f649106ebaf4580
Francisco Ramírez <frankjose2004@gmail.com>
2025-08-03 16:23:10 -0500

    FIX: Request redirect;

--- frontend/src/api/patients/getPatientsCount.ts
+++ frontend/src/api/patients/getPatientsCount.ts
@@ -1,7 +1,7 @@
 import { apiPublic } from '../config'
 
 export const getPatientsCount = async () => {
-	const { data } = await apiPublic.get<{ count: number }>('/users/count/')
+	const { data } = await apiPublic.get<{ count: number }>('/users/count')
 
 	return data
 }
