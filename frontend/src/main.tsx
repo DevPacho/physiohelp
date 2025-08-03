ec06a1954005a500c4cd24fcb4fa110b2f24e313
Francisco Ramírez <frankjose2004@gmail.com>
2025-08-03 07:05:37 -0500

    CREATE: Router provider;

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,16 @@
 import { StrictMode } from 'react'
 
+import { Router } from '@router'
+
 import '@styles/globals.css'
 
 import { createRoot } from 'react-dom/client'
 import { Toaster } from 'react-hot-toast'
+import { RouterProvider } from 'react-router'
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
+		<RouterProvider router={Router} />
 		<Toaster
 			position='top-right'
 			reverseOrder={false}
---
+++ frontend/src/router/Router.tsx
@@ -0,0 +1,29 @@
+import { MainLayout } from '@components/layouts'
+import { Navigate, createBrowserRouter } from 'react-router'
+
+export const Router = createBrowserRouter([
+	{
+		path: '/',
+		element: <MainLayout />,
+		errorElement: <h1>Error page - PhysioHelp</h1>,
+		children: [
+			{
+				index: true,
+				path: 'patients',
+				element: <>Pacientes</>,
+			},
+			{
+				path: 'medical-records',
+				element: <>Historias Clínicas</>,
+			},
+			{
+				path: '/',
+				element: <Navigate to='/patients' replace />,
+			},
+		],
+	},
+	{
+		path: '*',
+		element: <Navigate to='/patients' replace />,
+	},
+])
