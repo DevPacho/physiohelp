09236cdadcff5b829d3eb2137204ab742afd02a9
Francisco Ramírez <frankjose2004@gmail.com>
2025-08-06 02:45:18 -0500

    CREATE: Modal component to delete patients;

---
+++ frontend/src/components/organisms/DeletePatientModal/DeletePatientModal.tsx
@@ -0,0 +1,42 @@
+import { IPatient } from '@interfaces'
+
+import { Button } from '@components/atoms'
+
+interface IDeletePatientModalProps {
+	patient: IPatient
+	isDeletingPatient: boolean
+	deletePatient: (patientId: number) => Promise<void>
+}
+
+export const DeletePatientModal = ({
+	patient,
+	isDeletingPatient,
+	deletePatient,
+}: IDeletePatientModalProps) => {
+	const handleDelete = () => deletePatient(patient.id)
+
+	return (
+		<div className='mt-5'>
+			<p className='text-sm'>
+				¿Está segura de que desea eliminar a{' '}
+				<span className='font-semibold text-black capitalize'>
+					{patient.name} {patient.last_name}
+				</span>{' '}
+				como paciente?
+			</p>
+			<p className='text-sm'>
+				Esta acción no se puede deshacer. Se eliminarán todos sus datos
+				asociados.
+			</p>
+			<footer className='flex justify-end gap-3 pt-4'>
+				<Button
+					text={
+						isDeletingPatient ? 'Eliminando Paciente...' : 'Eliminar Paciente'
+					}
+					onClick={handleDelete}
+					disabled={isDeletingPatient}
+				/>
+			</footer>
+		</div>
+	)
+}
---
+++ frontend/src/components/organisms/DeletePatientModal/index.ts
@@ -0,0 +1,1 @@
+export { DeletePatientModal } from './DeletePatientModal'
