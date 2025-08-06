e2c1c703f21f2e4e7a60b68228ab688c7b15b43b
Francisco Ramírez <frankjose2004@gmail.com>
2025-08-06 01:37:00 -0500

    CHANGE: Request method;

--- frontend/src/api/patients/updatePatient.ts
+++ frontend/src/api/patients/updatePatient.ts
@@ -11,7 +11,7 @@ export const updatePatient = async ({
 	patientId,
 	patientData,
 }: IUpdatePatientParams): Promise<IPatient> => {
-	const { data } = await apiPublic.patch(`/users/${patientId}/`, patientData)
+	const { data } = await apiPublic.put(`/users/${patientId}`, patientData)
 
 	return data
 }
