c12b25338cf1e3d5bc2fc8f3efbfc8840ddbbfb8
Francisco Ramírez <frankjose2004@gmail.com>
2025-08-06 00:35:41 -0500

    ADD: Search param;

--- frontend/src/api/patients/getPatients.ts
+++ frontend/src/api/patients/getPatients.ts
@@ -5,13 +5,19 @@ import { apiPublic } from '../config'
 interface IGetPatientsParams {
 	skip: number
 	limit: number
+	search?: string
 }
 
-export const getPatients = async ({ skip, limit }: IGetPatientsParams) => {
+export const getPatients = async ({
+	skip,
+	limit,
+	search,
+}: IGetPatientsParams) => {
 	const { data } = await apiPublic.get<IPatient[]>('/users/', {
 		params: {
 			skip,
 			limit,
+			search,
 		},
 	})
 
--- frontend/src/api/patients/getPatientsCount.ts
+++ frontend/src/api/patients/getPatientsCount.ts
@@ -1,7 +1,15 @@
 import { apiPublic } from '../config'
 
-export const getPatientsCount = async () => {
-	const { data } = await apiPublic.get<{ count: number }>('/users/count')
+interface IGetPatientsCountParams {
+	search?: string
+}
+
+export const getPatientsCount = async ({ search }: IGetPatientsCountParams) => {
+	const { data } = await apiPublic.get<{ count: number }>('/users/count', {
+		params: {
+			search,
+		},
+	})
 
 	return data
 }
