4faece392ad627b41403c49cbefbef75243570bf
Francisco Ramírez <frankjose2004@gmail.com>
2025-08-07 18:25:37 -0500

    ADD: Props tu customize filename;

--- frontend/src/api/medicalRecords/generateAndDownloadMedicalRecordPdf.ts
+++ frontend/src/api/medicalRecords/generateAndDownloadMedicalRecordPdf.ts
@@ -2,10 +2,16 @@ import { apiPublic } from '../config'
 
 interface IGenerateAndDownloadMedicalRecordPdfParams {
 	patientId: number
+	patientFullName: string
+	agreementType: string
+	date: string
 }
 
 export const generateAndDownloadMedicalRecordPdf = async ({
 	patientId,
+	patientFullName,
+	agreementType,
+	date,
 }: IGenerateAndDownloadMedicalRecordPdfParams): Promise<void> => {
 	const response = await apiPublic.post(
 		`/users/${patientId}/generate-pdf`,
@@ -19,7 +25,10 @@ export const generateAndDownloadMedicalRecordPdf = async ({
 	const link = document.createElement('a')
 
 	link.href = url
-	link.setAttribute('download', '')
+	link.setAttribute(
+		'download',
+		`${agreementType} - ${patientFullName} - ${date}.pdf`
+	)
 	document.body.appendChild(link)
 	link.click()
 	link.remove()
