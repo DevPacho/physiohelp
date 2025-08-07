b9343e70f1eabe21cd5c282f6eb1ba135c45d614
Francisco Ramírez <frankjose2004@gmail.com>
2025-08-07 18:25:09 -0500

    ADD: Title prop;

--- frontend/src/components/atoms/Button/Button.tsx
+++ frontend/src/components/atoms/Button/Button.tsx
@@ -2,14 +2,16 @@ import { MouseEvent } from 'react'
 
 interface IButtonProps {
 	text: string
+	title?: string
 	onClick: (event: MouseEvent<HTMLButtonElement>) => void
 	disabled?: boolean
 }
 
-export const Button = ({ text, onClick, disabled }: IButtonProps) => (
+export const Button = ({ text, title, onClick, disabled }: IButtonProps) => (
 	<button
 		type='button'
-		className='bg-primary-light h-10 cursor-pointer rounded-md px-4 text-white hover:underline disabled:cursor-not-allowed disabled:opacity-50 disabled:hover:no-underline text-nowrap'
+		title={title}
+		className='bg-primary-light h-10 cursor-pointer rounded-md px-4 text-nowrap text-white hover:underline disabled:cursor-not-allowed disabled:opacity-50 disabled:hover:no-underline'
 		onClick={onClick}
 		disabled={disabled}
 	>
