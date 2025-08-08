8c3ae01f3c79edccc1f2d0402eab5b242d327f28
Francisco Ramírez <frankjose2004@gmail.com>
2025-08-07 22:46:24 -0500

    CREATE Patient evolution tab component;

---
+++ frontend/src/components/organisms/PatientEvolutionTab/PatientEvolutionTab.tsx
@@ -0,0 +1,129 @@
+import { useState } from 'react'
+
+import { IEvolution } from '@interfaces'
+
+import { Button } from '@components/atoms'
+import { EvolutionItem, Modal } from '@components/molecules'
+
+import { DeleteEvolutionModal } from '../DeleteEvolutionModal/DeleteEvolutionModal'
+import { EvolutionModal } from '../EvolutionModal/EvolutionModal'
+
+interface IPatientEvolutionTabProps {
+	isLoading: boolean
+	evolutions: IEvolution[]
+	handleCreateEvolution: (
+		evolutionData: Omit<IEvolution, 'id' | 'medical_record_id'>
+	) => Promise<void>
+	handleUpdateEvolution: (
+		evolutionId: number,
+		evolutionData: Partial<IEvolution>
+	) => Promise<void>
+	handleDeleteEvolution: (evolutionId: number) => Promise<void>
+}
+
+export const PatientEvolutionTab = ({
+	isLoading,
+	evolutions,
+	handleCreateEvolution,
+	handleUpdateEvolution,
+	handleDeleteEvolution,
+}: IPatientEvolutionTabProps) => {
+	const [showCreateEvolutionModal, setShowCreateEvolutionModal] =
+		useState(false)
+	const [selectedEvolution, setSelectedEvolution] = useState<IEvolution | null>(
+		null
+	)
+	const [selectedEvolutionToDelete, setSelectedEvolutionToDelete] =
+		useState<IEvolution | null>(null)
+
+	return (
+		<>
+			{evolutions.length === 0 ? (
+				<div className='flex flex-col justify-between gap-4 md:flex-row md:items-center'>
+					<p className='text-[15px]'>
+						No se ha encontrado ninguna evolución asociada a este paciente.
+					</p>
+					<Button
+						text='Crear Evolución'
+						onClick={() => setShowCreateEvolutionModal(true)}
+					/>
+				</div>
+			) : (
+				<div className='flex flex-col gap-4'>
+					<div className='flex flex-col justify-between gap-4 md:flex-row md:items-center'>
+						<div className='flex flex-col gap-1'>
+							<h2 className='min-w-fit text-xl font-semibold text-black'>
+								Evoluciones
+							</h2>
+							<h3 className='text-sm text-black'>
+								Registro de evoluciones del paciente.
+							</h3>
+						</div>
+						<Button
+							text='Crear Evolución'
+							onClick={() => setShowCreateEvolutionModal(true)}
+						/>
+					</div>
+					{evolutions.map((evolution, evolutionIndex) => (
+						<EvolutionItem
+							key={evolution.id}
+							{...{
+								evolution,
+								evolutionIndex,
+								setSelectedEvolution,
+								setSelectedEvolutionToDelete,
+							}}
+						/>
+					))}
+				</div>
+			)}
+			{showCreateEvolutionModal && (
+				<Modal
+					title='Crear Evolución'
+					subtitle='Diligencia todos los campos para crear la evolución.'
+					isOpen={showCreateEvolutionModal}
+					onClose={() => setShowCreateEvolutionModal(false)}
+					modalContentClassName='w-full xl:min-w-[600px] xl:w-fit'
+				>
+					<EvolutionModal
+						isLoading={isLoading}
+						onSubmit={handleCreateEvolution}
+					/>
+				</Modal>
+			)}
+			{selectedEvolution && (
+				<Modal
+					title='Actualizar Evolución'
+					subtitle='Modifica los campos necesarios para actualizar la evolución.'
+					isOpen={!!selectedEvolution}
+					onClose={() => setSelectedEvolution(null)}
+					modalContentClassName='w-full xl:min-w-[600px] xl:w-fit'
+				>
+					<EvolutionModal
+						isLoading={isLoading}
+						isEdit={!!selectedEvolution}
+						evolution={selectedEvolution}
+						onSubmit={evolutionData =>
+							handleUpdateEvolution(selectedEvolution.id, evolutionData)
+						}
+					/>
+				</Modal>
+			)}
+			{selectedEvolutionToDelete && (
+				<Modal
+					title='Eliminar Evolución'
+					subtitle='Confirma la eliminación de la evolución.'
+					isOpen={!!selectedEvolutionToDelete}
+					onClose={() => setSelectedEvolutionToDelete(null)}
+					modalContentClassName='w-full xl:min-w-[400px] xl:w-fit'
+				>
+					<DeleteEvolutionModal
+						evolution={selectedEvolutionToDelete}
+						isDeletingEvolution={isLoading}
+						deleteEvolution={handleDeleteEvolution}
+					/>
+				</Modal>
+			)}
+		</>
+	)
+}
---
+++ frontend/src/components/organisms/PatientEvolutionTab/index.ts
@@ -0,0 +1,1 @@
+export { PatientEvolutionTab } from './PatientEvolutionTab'
