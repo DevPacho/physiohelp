d5968b92c05c992573b927165e9d466fd25cafb3
Francisco Ramírez <frankjose2004@gmail.com>
2025-08-08 15:55:59 -0500

    CREATE: Menu chevron icon;

--- frontend/src/components/atoms/Icons/index.ts
+++ frontend/src/components/atoms/Icons/index.ts
@@ -1,5 +1,6 @@
 export { IcChevron } from './IcChevron'
 export { IcMenu } from './IcMenu'
+export { IcMenuChevron } from './IcMenuChevron'
 export { IcPencil } from './IcPencil'
 export { IcSpinner } from './IcSpinner'
 export { IcTrash } from './IcTrash'
