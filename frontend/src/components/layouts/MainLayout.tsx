b2b52a34e8a4457765e00f910cef5e4b10b48326
Francisco Ramírez <frankjose2004@gmail.com>
2025-08-08 16:00:41 -0500

    ADD: Collapse sidebar;

--- frontend/src/components/layouts/MainLayout.tsx
+++ frontend/src/components/layouts/MainLayout.tsx
@@ -5,15 +5,16 @@ import { Outlet } from 'react-router'
 
 export const MainLayout = () => {
 	const [showSidebar, setShowSidebar] = useState<boolean>(false)
+	const [collapseSidebar, setCollapseSidebar] = useState<boolean>(false)
 
 	return (
 		<div className='flex h-svh w-svw shrink-0 overflow-hidden bg-[#E4F8FF] xl:h-screen xl:max-h-screen'>
-			<MainSideBar {...{ showSidebar, setShowSidebar }} />
+			<MainSideBar {...{ collapseSidebar, showSidebar, setShowSidebar }} />
 			{showSidebar && (
 				<button
 					type='button'
 					onClick={() => setShowSidebar(false)}
-					className='text-black absolute top-4 right-4 z-10 cursor-pointer text-3xl font-semibold xl:hidden'
+					className='absolute top-4 right-4 z-10 cursor-pointer text-3xl font-semibold text-black xl:hidden'
 				>
 					X
 				</button>
@@ -22,7 +23,9 @@ export const MainLayout = () => {
 				className={`flex flex-1 flex-col overflow-auto ${showSidebar && 'blur-sm xl:blur-none'}`}
 				onClick={() => showSidebar && setShowSidebar(false)}
 			>
-				<MainNavBar {...{ setShowSidebar }} />
+				<MainNavBar
+					{...{ collapseSidebar, setCollapseSidebar, setShowSidebar }}
+				/>
 				<Outlet />
 			</section>
 		</div>
--- frontend/src/components/organisms/MainSideBar/MainSideBar.tsx
+++ frontend/src/components/organisms/MainSideBar/MainSideBar.tsx
@@ -1,18 +1,18 @@
-import { Dispatch, SetStateAction, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 
-import { PhysioHelpLogo } from '@assets'
+import { PhysioHelpIcon, PhysioHelpLogo } from '@assets'
 
 import { IcUser } from '@components/atoms'
 import { Link, useLocation } from 'react-router'
 
 interface IMainSideBarProps {
+	collapseSidebar: boolean
 	showSidebar: boolean
-	setShowSidebar: Dispatch<SetStateAction<boolean>>
 }
 
 export const MainSideBar = ({
+	collapseSidebar,
 	showSidebar,
-	setShowSidebar,
 }: IMainSideBarProps) => {
 	const [activeTab, setActiveTab] = useState<string>('')
 
@@ -21,30 +21,52 @@ export const MainSideBar = ({
 	useEffect(() => {
 		if (pathname.includes('patients')) setActiveTab('patients')
 		if (pathname.includes('medical-records')) setActiveTab('medical-records')
-
-		setShowSidebar(false)
 	}, [pathname])
 
 	return (
 		<aside
 			className={`${
 				showSidebar ? 'translate-x-0' : '-translate-x-full'
-			} bg-primary fixed z-10 flex h-full w-[300px] flex-col items-center shadow transition-all duration-300 ease-in-out xl:static xl:translate-x-0`}
+			} bg-primary fixed z-10 flex h-full flex-col items-center shadow transition-all duration-300 ease-in-out xl:static xl:translate-x-0 ${
+				collapseSidebar ? 'xl:w-20' : 'xl:w-[300px]'
+			} w-[300px]`}
 		>
-			<img
-				src={PhysioHelpLogo}
-				className='h-24 min-h-24 w-full object-center'
-				alt='PhysioHelp Logo'
-			/>
+			{collapseSidebar ? (
+				<img
+					src={PhysioHelpIcon}
+					className={`h-24 min-h-24 object-center transition-all duration-300 ${
+						collapseSidebar && !showSidebar ? 'w-12 object-contain' : 'w-full'
+					}`}
+					alt='PhysioHelp Icon'
+				/>
+			) : (
+				<img
+					src={PhysioHelpLogo}
+					className={`h-24 min-h-24 object-center transition-all duration-300 ${
+						collapseSidebar && !showSidebar
+							? 'xl:w-12 xl:object-contain'
+							: 'w-full'
+					}`}
+					alt='PhysioHelp Logo'
+				/>
+			)}
 			<nav className='flex h-full w-full flex-col text-white *:flex *:h-14 *:items-center *:gap-3 xl:text-lg'>
 				<Link
 					to='/patients'
-					className={`group hover:bg-primary-light px-10 ${
-						activeTab === 'patients' && 'bg-primary-light'
-					}`}
+					className={`group hover:bg-primary-light ${
+						collapseSidebar && !showSidebar
+							? 'xl:justify-center xl:px-2'
+							: 'px-10'
+					} ${activeTab === 'patients' && 'bg-primary-light'}`}
 				>
-					<IcUser className='size-5 fill-white' />
-					<p className='line-clamp-1 group-hover:underline'>Pacientes</p>
+					<IcUser className='size-5 flex-shrink-0 fill-white' />
+					<p
+						className={`line-clamp-1 transition-all duration-300 group-hover:underline ${
+							collapseSidebar && !showSidebar ? 'xl:hidden' : ''
+						}`}
+					>
+						Pacientes
+					</p>
 				</Link>
 			</nav>
 		</aside>
